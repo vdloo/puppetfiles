782c26f859729ccee698d791149ea3fd66712939
Rick van de Loo <rickvandeloo@gmail.com>
2015-05-26 03:46:04 +1000

    apt-get upgrade noconfirm

--- operating_systems/debianlike/manifests/init.pp
+++ operating_systems/debianlike/manifests/init.pp
@@ -8,8 +8,8 @@ class update_apt {
     exec { 'apt-get update':
 	command => '/usr/bin/apt-get update'
     }
-    exec { 'apt-get upgrade':
-	command => '/usr/bin/apt-get upgrade'
+    exec { 'apt-get upgrade -y':
+	command => '/usr/bin/apt-get upgrade -y'
     }
 }
 
