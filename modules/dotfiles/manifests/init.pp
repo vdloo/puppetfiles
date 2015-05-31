3c18c54a0472a82a73a3d21ad2ac8c93a29189aa
Rick van de Loo <rickvandeloo@gmail.com>
2015-05-31 12:32:14 -0500

    link dotfiles

--- modules/dotfiles/manifests/init.pp
+++ modules/dotfiles/manifests/init.pp
@@ -6,4 +6,24 @@ class dotfiles {
       user => 'vdloo',
       owner => 'vdloo',
     }
+    include linkdotfiles
+}
+
+class linkdotfiles {
+    require dotfiles
+    file { '/home/vdloo/.bashrc':
+	ensure => 'link',
+	target => '/home/vdloo/.dotfiles/.bashrc',
+	owner => 'vdloo',
+    }
+    file { '/home/vdloo/.profile':
+	ensure => 'link',
+	target => '/home/vdloo/.dotfiles/.profile',
+	owner => 'vdloo',
+    }
+    file { '/home/vdloo/.Xdefaults':
+	ensure => 'link',
+	target => '/home/vdloo/.dotfiles/.Xdefaults',
+	owner => 'vdloo',
+    }
 }
