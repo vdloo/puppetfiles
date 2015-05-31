ccee9e90002ad8c8e47fe4f8b03f8cddfedc2b1c
Rick van de Loo <rickvandeloo@gmail.com>
2015-05-31 12:47:32 -0500

    set git config

--- modules/nonroot/manifests/init.pp
+++ modules/nonroot/manifests/init.pp
@@ -1,9 +1,24 @@
 class nonroot {
+    include createnonrootuser
+    include setgitconfig
+}
+
+class createnonrootuser {
     user { 
 	'vdloo':
 	ensure => present,
 	shell => '/bin/bash',
 	managehome => 'true',
-	password => generate('/bin/sh', '-c', "mkpasswd -m sha-512 'toor' | tr -d '\n'"),
+	password => generate('/bin/sh', '-c', "mkpasswd -m sha-512 toor | tr -d '\n'"),
+    }
+}
+
+class setgitconfig {
+    require createnonrootuser
+    exec { 'set git user email':
+	command => '/bin/su - vdloo -c "/usr/bin/git config --global user.email \'rickvandeloo@gmail.com\'"',
+    }
+    exec { 'set git user name':
+	command => '/bin/su - vdloo -c "/usr/bin/git config --global user.name \'Rick van de Loo\'"',
     }
 }
