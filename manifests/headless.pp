3c03407450715789bdba84d4fd62c5a6da24a8a4
Rick van de Loo <rickvandeloo@gmail.com>
2015-06-01 07:32:22 -0500

    cron puppetruns

--- manifests/headless.pp
+++ manifests/headless.pp
@@ -1,1 +1,11 @@
 include common
+include headless_cron
+
+class headless_cron {
+    cron { "puppet apply":
+        command => "/usr/etc/puppetfiles/papply.sh /usr/etc/puppetfiles/manifests/headless.pp",
+        user    => "root",
+        hour    => 0,
+        minute  => 15
+    }
+}
--- manifests/workstation.pp
+++ manifests/workstation.pp
@@ -1,2 +1,12 @@
 include common
 include workstation
+include workstation_cron
+
+class workstation_cron {
+    cron { "puppet apply":
+        command => "/usr/etc/puppetfiles/papply.sh /usr/etc/puppetfiles/manifests/workstation.pp",
+        user    => "root",
+        hour    => 0,
+        minute  => 15
+    }
+}
