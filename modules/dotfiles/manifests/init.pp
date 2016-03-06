b0c69d1942f1dc19cbe924c7d9e07de324145ce4
Rick van de Loo <rickvandeloo@gmail.com>
2016-03-06 13:23:22 +0100

    link terminator config to home .config dir

--- modules/dotfiles/manifests/init.pp
+++ modules/dotfiles/manifests/init.pp
@@ -1,34 +1,49 @@
 class dotfiles {
-    vcsrepo { "/home/${::nonroot_username}/.dotfiles":
-	ensure   => latest,
-	provider => git,
-	source => 'git://github.com/vdloo/dotfiles.git',
-	user => $::nonroot_username,
-	owner => $::nonroot_username,
-    }
-    include linkdotfiles
+	vcsrepo { "/home/${::nonroot_username}/.dotfiles":
+		ensure   => latest,
+		provider => git,
+		source => 'git://github.com/vdloo/dotfiles.git',
+		user => $::nonroot_username,
+		owner => $::nonroot_username,
+	}
+	include linkdotfiles
 }
 
 class linkdotfiles {
-    require dotfiles
-    file { "/home/${::nonroot_username}/.bashrc":
-	ensure => 'link',
-	target => "/home/${::nonroot_username}/.dotfiles/.bashrc",
-	owner => $::nonroot_username,
-    }
-    file { "/home/${::nonroot_username}/.profile":
-	ensure => 'link',
-	target => "/home/${::nonroot_username}/.dotfiles/.profile",
-	owner => $::nonroot_username,
-    }
-    file { "/home/${::nonroot_username}/.Xdefaults":
-	ensure => 'link',
-	target => "/home/${::nonroot_username}/.dotfiles/.Xdefaults",
-	owner => $::nonroot_username,
-    }
-    file { "/home/${::nonroot_username}/.xinitrc":
-	ensure => 'link',
-	target => "/home/${::nonroot_username}/.dotfiles/.xinitrc",
-	owner => $::nonroot_username,
-    }
+	require dotfiles
+	file { "/home/${::nonroot_username}/.bashrc":
+		ensure => 'link',
+		target => "/home/${::nonroot_username}/.dotfiles/.bashrc",
+		owner => $::nonroot_username,
+	}
+	file { "/home/${::nonroot_username}/.profile":
+		ensure => 'link',
+		target => "/home/${::nonroot_username}/.dotfiles/.profile",
+		owner => $::nonroot_username,
+	}
+	file { "/home/${::nonroot_username}/.Xdefaults":
+		ensure => 'link',
+		target => "/home/${::nonroot_username}/.dotfiles/.Xdefaults",
+		owner => $::nonroot_username,
+	}
+	file { "/home/${::nonroot_username}/.xinitrc":
+		ensure => 'link',
+		target => "/home/${::nonroot_username}/.dotfiles/.xinitrc",
+		owner => $::nonroot_username,
+	}
+	file { "/home/${::nonroot_username}/.config":
+		ensure => 'directory',
+		mode => '0755',
+		owner => $::nonroot_username,
+	}
+	file { "/home/${::nonroot_username}/.config/terminator":
+		ensure => 'directory',
+		mode => '0755',
+		owner => $::nonroot_username,
+	}
+	file { "/home/${::nonroot_username}/.config/terminator/config":
+		ensure => 'link',
+		target => "/home/${::nonroot_username}/.dotfiles/.config/terminator/config",
+		owner => $::nonroot_username,
+	}
 }
