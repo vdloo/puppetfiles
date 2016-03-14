88ec10e4bdeb6f30c7d2bc25b253d8c6f704ed61
Rick van de Loo <rickvandeloo@gmail.com>
2016-03-14 23:05:58 +0100

    only rank european mirrors

--- operating_systems/archlinux/manifests/init.pp
+++ operating_systems/archlinux/manifests/init.pp
@@ -6,24 +6,32 @@ class download_mirrorlist {
 	require wget
 	wget::fetch { 'download archlinux mirrorlist':
 		source => 'https://www.archlinux.org/mirrorlist/all/',
-		destination => "/home/${::nonroot_username}/.mirrorlist",
+		destination => "/home/${::nonroot_username}/.full_mirrorlist",
 		timeout => 0,
 		verbose => false,
 		execuser => $::nonroot_username,
 	}
 }
 
-class ensure_mirrorlist {
+class ensure_global_mirrorlist {
 	require download_mirrorlist
 	exec { 'uncomment mirrors':
-		command => "/usr/bin/sed -i 's/^#Server/Server/' /home/${::nonroot_username}/.mirrorlist",
+		command => "/usr/bin/sed -e 's/^#Server/Server/' /home/${::nonroot_username}/.full_mirrorlist > /home/${::nonroot_username}/.all_mirrors",
+	}
+}
+
+class ensure_european_mirrorlist {
+        require ensure_global_mirrorlist
+	exec { 'get european mirrors':
+		command => "/usr/bin/grep 'nl\\/\\|eu\\/\\|\\.be\\/\\|\\.de\\/' /home/${::nonroot_username}/.all_mirrors > /home/${::nonroot_username}/.european_mirrors",
 	}
 }
 
 class get_fastest_mirrors {
-	require ensure_mirrorlist
+	require ensure_european_mirrorlist
 	exec { 'rank mirrors':
-		command => "/usr/bin/rankmirrors -n 3 /home/${::nonroot_username}/.mirrorlist > /etc/pacman.d/mirrorlist",
+		command => "/usr/bin/rankmirrors -n 3 /home/${::nonroot_username}/.european_mirrors > /etc/pacman.d/mirrorlist",
+		timeout => 600
 	}
 }
 
